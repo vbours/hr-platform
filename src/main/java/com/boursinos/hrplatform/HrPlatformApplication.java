9be6aaa99ee0e933295305dd565b67a827a1cc0b
v.boursinos <v.boursinos@turintech.ai>
2022-11-26 11:19:25 +0000

    feat: create project

---
+++ src/main/java/com/boursinos/hrplatform/HrPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.boursinos.hrplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boursinos/hrplatform/HrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boursinos.hrplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
